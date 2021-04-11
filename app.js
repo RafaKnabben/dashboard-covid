138369ce1ed773b937a38cee0658f58bd30de0c1
Rafael Knabben <rknabben@gmail.com>
2021-04-11 22:12:23 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -5,23 +5,30 @@ const newDeathsBra = document.querySelector("#newDeathsBra");
 const deathBra = document.querySelector("#deathBRA");
 const activeBra = document.querySelector("#activeBRA");
 const listCountries = document.querySelector("#countries-list");
+const incidence = document.querySelector("#incidence");
 
 const setCountryAPI = function () {
     const list = axios(`https://api.covid19api.com/countries`);
     return list;
 }
 
+const testAPI = async function () {
+    const sel = await axios(`https://restcountries.eu/rest/v2/alpha/mg`);
+    const pops = sel.data.population;
+    return pops;
+}
+
 const setCountriesList = async function () {
     const fetchCountries = await setCountryAPI();
     const arrCountries = fetchCountries.data;
     
-    try { 
+    try {
         arrCountries.forEach((i) => {
-            const item = document.createElement("option");
-            item.setAttribute("class", "list-item");
-            item.setAttribute("value", `${i.Slug}`);
-            item.append(i.Country);
-            listCountries.appendChild(item);
+                const item = document.createElement("option");
+                item.setAttribute("class", "list-item");
+                item.setAttribute("value", `${i.ISO2}`);
+                item.append(i.Country);
+                listCountries.appendChild(item);
         });
     } catch {
         console.error("Apparently it is not possible to list the countries.");
@@ -33,47 +40,31 @@ setCountriesList();
 window.addEventListener("click", async (e) => {
     e.preventDefault();
 
-    const option = listCountries.options[listCountries.selectedIndex].value;
+    const countryPick = listCountries.options[listCountries.selectedIndex].value;
 
-    const sel = await axios(`https://api.covid19api.com/total/country/${option}`);
+    const sel = await axios(`https://api.covid19api.com/total/country/${countryPick}`);
+    const selPops = await axios(`https://restcountries.eu/rest/v2/alpha/${countryPick}`)
 
-    const totalCasesBra = sel.data[sel.data.length -1].Confirmed;
-    const casesNewBra = totalCasesBra - sel.data[sel.data.length -2].Confirmed;
-    const totalDeathBra = sel.data[sel.data.length -1].Deaths;
-    const deathsNewBra = totalDeathBra - sel.data[sel.data.length -2].Deaths;
-    const totalActiveBra = sel.data[sel.data.length -1].Active;
-
-    selectedCountry.innerHTML = `${sel.data[sel.data.length -1].Country}`;
-    newCasesBra.innerHTML = `<p>${casesNewBra}</p>`;
-    totalBra.innerHTML = `<p>${totalCasesBra}</p>`;
-    newDeathsBra.innerHTML = `<p>${deathsNewBra}</p>`;
-    deathBra.innerHTML = `<p>${totalDeathBra}</p>`;
-    activeBra.innerHTML = `<p>${totalActiveBra}</p>`;
-    // return console.log(option);
+    try {
+        const pops = selPops.data.population / 100000;
+        const totalCasesBra = sel.data[sel.data.length -1].Confirmed;
+        const casesNewBra = totalCasesBra - sel.data[sel.data.length -2].Confirmed;
+        const lastWeek = casesNewBra + (sel.data[sel.data.length -2].Confirmed - sel.data[sel.data.length -3].Confirmed) + (sel.data[sel.data.length -3].Confirmed - sel.data[sel.data.length -4].Confirmed) + (sel.data[sel.data.length -4].Confirmed - sel.data[sel.data.length -5].Confirmed) + (sel.data[sel.data.length -5].Confirmed - sel.data[sel.data.length -6].Confirmed) + (sel.data[sel.data.length -6].Confirmed - sel.data[sel.data.length -7].Confirmed) + (sel.data[sel.data.length -7].Confirmed - sel.data[sel.data.length -8].Confirmed);
+        const totalDeathBra = sel.data[sel.data.length -1].Deaths;
+        const deathsNewBra = totalDeathBra - sel.data[sel.data.length -2].Deaths;
+        const LastWeekDeaths = deathsNewBra + (sel.data[sel.data.length -2].Deaths - sel.data[sel.data.length -3].Deaths) + (sel.data[sel.data.length -3].Deaths - sel.data[sel.data.length -4].Deaths) + (sel.data[sel.data.length -4].Deaths - sel.data[sel.data.length -5].Deaths) + (sel.data[sel.data.length -5].Deaths - sel.data[sel.data.length -6].Deaths) + (sel.data[sel.data.length -6].Deaths - sel.data[sel.data.length -7].Deaths) + (sel.data[sel.data.length -7].Deaths - sel.data[sel.data.length -8].Deaths);
+        const totalActiveBra = sel.data[sel.data.length -1].Active;
+        const currentIncidence = Math.round(lastWeek / pops);
+        const currentDeathIncidence = Math.round(LastWeekDeaths / pops);
+    
+        selectedCountry.innerHTML = `${sel.data[sel.data.length -1].Country}`;
+        incidence.innerHTML = `<p>${currentIncidence}</p><p>${currentDeathIncidence}</p>`;
+        newCasesBra.innerHTML = `<p>${casesNewBra}</p>`;
+        totalBra.innerHTML = `<p>${totalCasesBra}</p>`;
+        newDeathsBra.innerHTML = `<p>${deathsNewBra}</p>`;
+        deathBra.innerHTML = `<p>${totalDeathBra}</p>`;
+        activeBra.innerHTML = `<p>${totalActiveBra}</p>`;
+    } catch {
+        console.error("Country without data.")
+    }
 })
-
-
-// const fetchData = function () {
-//     const res = axios(`https://api.covid19api.com/total/country/brazil`);
-//     return res;
-// }
-
-// const selectData = async function () {
-
-//     const sel = await fetchData();
-
-//     const totalCasesBra = sel.data[sel.data.length -1].Confirmed;
-//     const casesNewBra = totalCasesBra - sel.data[sel.data.length -2].Confirmed;
-//     const totalDeathBra = sel.data[sel.data.length -1].Deaths;
-//     const deathsNewBra = totalDeathBra - sel.data[sel.data.length -2].Deaths;
-//     const totalActiveBra = sel.data[sel.data.length -1].Active;
-
-//     selectedCountry.innerHTML = `${sel.data[sel.data.length -1].Country}`;
-//     newCasesBra.innerHTML = `<p>${casesNewBra}</p>`;
-//     totalBra.innerHTML = `<p>${totalCasesBra}</p>`;
-//     newDeathsBra.innerHTML = `<p>${deathsNewBra}</p>`;
-//     deathBra.innerHTML = `<p>${totalDeathBra}</p>`;
-//     activeBra.innerHTML = `<p>${totalActiveBra}</p>`;
-// }
-
-// selectData();
